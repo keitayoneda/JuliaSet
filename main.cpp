5143e92647311b423272650715a0c0db9f8ba522
yoneda keita <yoneda-keita863@g.ecc.u-tokyo.ac.jp>
2023-07-13 02:29:37 +0900

    移動拡大とパラメータ変更を実装

--- main.cpp
+++ main.cpp
@@ -2,31 +2,106 @@
 #include <GLFW/glfw3.h>
 
 #include <array>
+#include <chrono>
 #include <cmath>
+#include <iomanip>
 #include <iostream>
+#include <thread>
 
 #include "shader.hpp"
 
 GLFWwindow *window;
 int width, height;
-constexpr int n_col = 512, n_raw = 256;
-float width_length = 2.0f;
-float x_min = -1.0f, x_max = 1.0f, y_min = -1.0f, y_max = 1.0f;
+constexpr int n_col = 768, n_raw = 1024;
+double width_length = 2.0;
+double height_length =
+    width_length * static_cast<float>(n_col) / static_cast<float>(n_raw);
+double zoom = 0.5;
+double x_center = -0.1197280330103155, y_center = 0.0769413973084643;
+double x_min = (-width_length / 2 + x_center) / zoom,
+       x_max = (width_length / 2 + x_center) / zoom,
+       y_min = (-height_length / 2 + y_center) / zoom,
+       y_max = (height_length / 2 + y_center) / zoom;
+
 std::array<std::array<float, 3 * 2 * n_raw>, n_col - 1> vertices;
 std::array<std::array<float, 3 * 2 * n_raw>, n_col - 1> colors;
+std::array<GLuint, n_col - 1> vertexbuffers;
 
-float c_x = 0.3f, c_y = -0.5f;
+constexpr int n_circle_division = 20;
+std::array<float, 3 * n_circle_division> circle_vertices;
+std::array<GLuint, n_col - 1> colorbuffers;
+GLuint programID;
 
-void recurence(float x, float y, float &next_x, float &next_y) {
+double c_x = 0.197, c_y = -0.558;
+bool draw_julia = false;
+
+int count = 0;
+
+void updateDrawArea() {
+  x_min = (-width_length / 2) / zoom + x_center;
+  x_max = (width_length / 2) / zoom + x_center;
+  y_min = (-height_length / 2) / zoom + y_center;
+  y_max = (height_length / 2) / zoom + y_center;
+}
+
+void mouse_cursor_callback(GLFWwindow *window, double x, double y) {
+  static double prev_x = x, prev_y = y;
+  if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE &&
+      glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_RELEASE) {
+    count++;
+
+    return;
+  }
+  if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS &&
+      glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_RELEASE) {
+    std::cout << "x: " << x << ", y: " << y << std::endl;
+    c_x = static_cast<double>(x) / static_cast<double>(width) * width_length -
+          width_length / 2.0;
+    c_y = static_cast<double>(height - y) / static_cast<double>(height) *
+              height_length -
+          height_length / 2.0;
+    std::cout << "c_x: " << c_x << ", c_y: " << c_y << std::endl;
+    draw_julia = true;
+    return;
+  }
+  if (count > 10) {
+    prev_x = x;
+    prev_y = y;
+    count = 0;
+  }
+  x_center -=
+      1 * (x - prev_x) / static_cast<double>(width) * width_length / zoom;
+  y_center -=
+      1 * (y - prev_y) / static_cast<double>(height) * height_length / zoom;
+
+  prev_x = x;
+  prev_y = y;
+  draw_julia = true;
+}
+
+void scroll_callback([[maybe_unused]] GLFWwindow *window,
+                     [[maybe_unused]] double x, double y) {
+  if (y > 0) {
+    zoom *= 1.1;
+  } else if (y < 0) {
+    zoom /= 1.1;
+  }
+  std::cout << std::setprecision(16) << "zoom: " << zoom
+            << ", x_center: " << x_center << ", y_center: " << y_center
+            << std::endl;
+  draw_julia = true;
+}
+
+void recurence(double x, double y, double &next_x, double &next_y) {
   next_x = x * x - y * y + c_x;
   next_y = 2 * x * y + c_y;
 }
 
-float calcGradient(float x0, float y0) {
-  const int max_iteration = 100;
-  float x = x0, y = y0;
+double calcGradient(double x0, double y0) {
+  const int max_iteration = 500;
+  double x = x0, y = y0;
   for (int i = 0; i < max_iteration; ++i) {
-    if (x * x + y * y > 4.0f) {
+    if (x * x + y * y > 4.0) {
       return static_cast<float>(i) / static_cast<float>(max_iteration);
     }
     recurence(x, y, x, y);
@@ -34,33 +109,52 @@ float calcGradient(float x0, float y0) {
   return 1.0f;
 }
 
-void calcColors(float x_min, float x_max, float y_min, float y_max) {
+void calcOnePixelColor(float gradient, float &r, float &g, float &b) {
+  r = -gradient * (gradient - 1.0) * 2;
+  g = gradient * (2.0 - gradient);
+  b = std::sqrt(gradient);
+}
+
+void calcColors(double x_min, double x_max, double y_min, double y_max) {
+  std::chrono::system_clock::time_point start, end;
+  start = std::chrono::system_clock::now();
+#pragma omp parallel for
   for (int i = 0; i < n_col; ++i) {
     for (int j = 0; j < n_raw; ++j) {
-      [[maybe_unused]] float x =
-          static_cast<float>(i) / static_cast<float>(n_col) * (x_max - x_min) +
-          x_min;
-      [[maybe_unused]] float y = static_cast<float>(n_raw - j) /
-                                     static_cast<float>(n_raw) *
-                                     (y_max - y_min) +
-                                 y_min;
-      /* float gradient = */
-      /*     static_cast<float>(i * n_raw + j) / static_cast<float>(n_col *
+      [[maybe_unused]] double x = static_cast<double>(i) /
+                                      static_cast<double>(n_col) *
+                                      (x_max - x_min) +
+                                  x_min;
+      [[maybe_unused]] double y = static_cast<double>(n_raw - j) /
+                                      static_cast<double>(n_raw) *
+                                      (y_max - y_min) +
+                                  y_min;
+      /* double gradient = */
+      /*     static_cast<double>(i * n_raw + j) / static_cast<double>(n_col *
        * n_raw); */
 
       float gradient = calcGradient(x, y);
+      float r, g, b;
+      calcOnePixelColor(gradient, r, g, b);
       if (i < n_col - 1) {
-        colors[i][6 * j] = 1 - 6 * std::cos(gradient);
-        colors[i][6 * j + 1] = std::sin(gradient);
-        colors[i][6 * j + 2] = gradient;
+        colors[i][6 * j] = r;
+        colors[i][6 * j + 1] = g;
+        colors[i][6 * j + 2] = b;
       }
       if (i > 0) {
-        colors[i - 1][6 * j + 3] = 1 - 6 * std::cos(gradient);
-        colors[i - 1][6 * j + 4] = std::sin(gradient);
-        colors[i - 1][6 * j + 5] = gradient;
+        colors[i - 1][6 * j + 3] = r;
+        colors[i - 1][6 * j + 4] = g;
+        colors[i - 1][6 * j + 5] = b;
       }
     }
   }
+  end = std::chrono::system_clock::now();
+  /* std::cout << "calcColors: " */
+  /*           << std::chrono::duration_cast<std::chrono::milliseconds>(end - */
+  /*                                                                    start)
+   */
+  /*                  .count() */
+  /*           << "ms" << std::endl; */
 }
 
 void initVertices() {
@@ -109,6 +203,8 @@ int init() {
   }
 
   glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+  glfwSetCursorPosCallback(window, mouse_cursor_callback);
+  glfwSetScrollCallback(window, scroll_callback);
 
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
@@ -117,7 +213,39 @@ int init() {
   return 0;
 }
 
-float draw_time = 0.0f;
+void drawJulia() {
+  updateDrawArea();
+  calcColors(x_min, x_max, y_min, y_max);
+  glfwGetFramebufferSize(window, &width, &height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glViewport(0, 0, width, height);
+  glOrtho(0, width, height, 0, -1, 1);
+
+  /* draw_time += 0.001f; */
+  /* c_x = 0.6f * std::cos(2 * draw_time); */
+  /* c_y = 0.6f * std::sin(2 * draw_time); */
+
+  glUseProgram(programID);
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+  for (int i = 0; i < n_col - 1; ++i) {
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffers[i]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffers[i]);
+    glBufferData(GL_ARRAY_BUFFER, colors[i].size() * sizeof(float), &colors[i],
+                 GL_DYNAMIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, n_raw * 2);
+  }
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+  std::cout << "zoom: " << zoom << std::endl;
+}
+
 int main() {
   if (init() != 0) {
     return -1;
@@ -127,10 +255,8 @@ int main() {
   glGenVertexArrays(1, &VertexArrayID);
   glBindVertexArray(VertexArrayID);
 
-  GLuint programID =
-      LoadShaders(SHADER_DIR "vertex.glsl", SHADER_DIR "fragment.glsl");
+  programID = LoadShaders(SHADER_DIR "vertex.glsl", SHADER_DIR "fragment.glsl");
 
-  std::array<GLuint, n_col - 1> vertexbuffers;
   for (int i = 0; i < n_col - 1; ++i) {
     glGenBuffers(1, &vertexbuffers[i]);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffers[i]);
@@ -138,7 +264,6 @@ int main() {
                  &vertices[i], GL_STATIC_DRAW);
   }
 
-  std::array<GLuint, n_col - 1> colorbuffers;
   for (int i = 0; i < n_col - 1; ++i) {
     glGenBuffers(1, &colorbuffers[i]);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffers[i]);
@@ -146,34 +271,33 @@ int main() {
                  GL_DYNAMIC_DRAW);
   }
 
+  drawJulia();
+  std::chrono::time_point<std::chrono::system_clock> start, end;
   do {
-    glfwGetFramebufferSize(window, &width, &height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glViewport(0, 0, width, height);
-    glOrtho(0, width, height, 0, -1, 1);
-
-    draw_time += 0.01f;
-    c_x = 0.6f * std::cos(2 * draw_time);
-    c_y = 0.6f * std::sin(2 * draw_time);
-    calcColors(x_min, x_max, y_min, y_max);
-
+    start = std::chrono::system_clock::now();
     glClear(GL_COLOR_BUFFER_BIT);
-
     glUseProgram(programID);
+    /* if (zoom < 1e12) { */
+    /*   zoom *= 1.05f; */
+    /*   draw_julia = true; */
+    /* } */
+    if (draw_julia) {
+      drawJulia();
+      draw_julia = false;
+    } else {
 
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    for (int i = 0; i < n_col - 1; ++i) {
-      glBindBuffer(GL_ARRAY_BUFFER, vertexbuffers[i]);
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+      glEnableVertexAttribArray(0);
+      glEnableVertexAttribArray(1);
+      for (int i = 0; i < n_col - 1; ++i) {
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffers[i]);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-      glBindBuffer(GL_ARRAY_BUFFER, colorbuffers[i]);
-      glBufferData(GL_ARRAY_BUFFER, colors[i].size() * sizeof(float),
-                   &colors[i], GL_DYNAMIC_DRAW);
-      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffers[i]);
 
-      glDrawArrays(GL_TRIANGLE_STRIP, 0, n_raw * 2);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, n_raw * 2);
+      }
     }
 
     glDisableVertexAttribArray(0);
@@ -181,6 +305,12 @@ int main() {
 
     glfwSwapBuffers(window);
     glfwPollEvents();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    if (elapsed_seconds.count() < 0.1) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(
+          static_cast<int>(1000 * (0.1 - elapsed_seconds.count()))));
+    }
   } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
            glfwWindowShouldClose(window) == 0);
 
